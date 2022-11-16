f11a4ce4d8273c0e7223f5bd96626bc380b66858
narkane <enikath@gmail.com>
2022-11-15 18:58:30 -0600

    fixed userModel to allow multiple "null" emails and added helmet and compression for prod

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: false,
-        index: true,
-        unique: true,
-        sparse: true,
+      type: String,
+      required: false,
+      index: true,
+      unique: true,
+      sparse: true,
     },
-    password: {type: String, required: false, minlength: 8},
-    firstName: {type: String, required: false},
-    lastName: {type: String, required: false},
-    email: {type: String, required: false, unique: true},
-});
+    password: { type: String, required: false, minlength: 8 },
+    firstName: { type: String, required: false },
+    lastName: { type: String, required: false },
+    email: { type: String, required: false, unique: true, sparse: true },
+  },
+  { timestamps: true, collection: "Users" }
+);
 
-module.exports = User = mongoose.model("user", userSchema, "users");
+module.exports = mongoose.model("user", userSchema);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 // const MongoClient = require("mongodb").MongoClient;
+
 
 // .env
 require("dotenv").config();
@@ -12,6 +15,8 @@ const DBURL = process.env.MONGO_URI;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(compression()))
 // app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
