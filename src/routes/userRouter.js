8263fa3a614caf108902e0868ee6bb5810e9019c
narkane <enikath@gmail.com>
2022-11-16 18:25:56 -0600

    tweaked user edit route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -109,8 +109,7 @@ router.post("/register", async (req, res) => {
 
 router.put("/edit", auth, async (req, res) => {
   try {
-    const { username, password, passwordCheck, firstName, lastName, email } =
-      req.body;
+    const { username, firstName, lastName, email } = req.body;
 
     userEdit = {};
 
@@ -118,32 +117,37 @@ router.put("/edit", auth, async (req, res) => {
     // 400 = bad request
     // 500 = internal server error
 
-    console.log("PUT /edit: " + req.body);
+    console.log("PUT /edit: " + JSON.stringify(req.body));
 
     // if (!username || !password || !passwordCheck || !firstName || !lastName || !email)) {
     //     return res.status(400).json({ msg: "Not all required fields have been entered." });
+    // }
+
+    // If not changing password then empty password field
+    // if (!passwordCheck) {
+    //   password = null;
     // }
 
     if (firstName) userEdit.firstName = firstName;
     if (lastName) userEdit.lastName = lastName;
 
     // Checking to ensure password length is at least 8 characters
-    if (password && password.length < 8) {
-      return res
-        .status(400)
-        .json({ msg: "Your password needs to be at least 8 characters long." });
-    }
+    // if (password && password.length < 8) {
+    //   return res
+    //     .status(400)
+    //     .json({ msg: "Your password needs to be at least 8 characters long." });
+    // }
 
     // Checking password entered vs the password checker
-    if (password && password !== passwordCheck) {
-      return res
-        .status(400)
-        .json({ msg: "The passwords entered do not match. Please try again." });
-    }
+    // if (password && password !== passwordCheck) {
+    //   return res
+    //     .status(400)
+    //     .json({ msg: "The passwords entered do not match. Please try again." });
+    // }
 
     // Checking DB for any existing user using the desired username
     if (username) {
-      console.log("ll");
+      // console.log("ll");
       const existingUsername = await User.findOne({ username: username });
       if (existingUsername) {
         return res
@@ -165,22 +169,33 @@ router.put("/edit", auth, async (req, res) => {
     }
 
     // Using bcrypt to hash passwords - for sekuritty, duh :P
-    if (password && passwordCheck) {
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt);
+    // if (password && passwordCheck) {
+    //   const salt = await bcrypt.genSalt();
+    //   const passwordHash = await bcrypt.hash(password, salt);
 
-      userEdit.password = passwordHash;
-    }
+    //   userEdit.password = passwordHash;
+    // }
 
-    console.log(userEdit);
+    console.log("new edit: ", userEdit);
+    console.log("req ID: ", req.body._id);
 
-    const editedUser = await User.findByIdAndUpdate(req.user, userEdit);
-    if (!editedUser) {
-      return res.status(400).json({
-        msg: "An account with this username already exists. (Q,Q ) Sad day.",
-      });
-    }
-    res.json(editedUser);
+    User.findOneAndUpdate(
+      req.body.id,
+      userEdit,
+      { new: true },
+      (err, result) => {
+        if (err) {
+          return res.status(400).json({
+            msg: `FAILED UPDATE`,
+          });
+        } else {
+          console.log("result: ", result);
+          // console.log("editedUser : ", editedUser);
+          // console.log(docs);
+          res.json(result);
+        }
+      }
+    );
   } catch (error) {
     res.status(500).json({ err: error.message });
   }
@@ -214,13 +229,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     res.json({
       token,
-      user: {
-        id: user._id,
-        username: user.username,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      },
+      user,
     });
   } catch (error) {
     res.status(500).json({ err: error.message });
@@ -249,13 +258,7 @@ router.post("/tokenIsValid", async (req, res) => {
     const user = await User.findById(verified.id);
     if (!user) return res.json(false);
 
-    return res.json({
-      email: user.email,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      username: user.username,
-      id: user._id,
-    });
+    return res.json(user);
   } catch (error) {
     res.status(500).json({ err: error.message });
   }
